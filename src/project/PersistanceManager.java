42051b367182223b169f215762035277da80cda4
Tiara <350704334@gapps.yrdsb.ca>
2026-01-12 09:46:05 -0500

    PersistanceManager class Code added
    - Description Added
    - Java libraries imported

--- src/project/PersistanceManager.java
+++ src/project/PersistanceManager.java
@@ -1,5 +1,18 @@
 package project;
 
+/* 
+ * Name: Tiara, William, Tarik
+ * Date: 2026.01.12
+ * Description: implementing the class to save and load feature
+ * creating the manager class
+ */
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class PersistanceManager {
+	
 
 }
