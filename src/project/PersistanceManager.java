4922390e2b0da55ac5760db7df74c0633583f767
Tiara <350704334@gapps.yrdsb.ca>
2026-01-12 09:56:08 -0500

    save completed
    The save method code is completed in PersistanceManager class.

--- src/project/PersistanceManager.java
+++ src/project/PersistanceManager.java
@@ -13,6 +13,23 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class PersistanceManager {
+
+	public static final String FILE_NAME = "schedule.txt";
+
+	// save a schedule code
+
+	public void save (Schedule scheduel) {
+		try {
+			FileWriter Writer = new FileWriter(FILE_NAME);
+			Writer.write(Schedule.getName());
+			Writer.close();
+		} catch(IOException e) {
+			System.out.print("Error saving schedule.");
+			
+		}
+	}
+
 	
+	//  Load a schedule code
 
 }
