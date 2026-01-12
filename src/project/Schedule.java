298956e3d9d3fb91a7afe3345f9edf3a4df71ff7
Tiara <350704334@gapps.yrdsb.ca>
2026-01-12 09:24:15 -0500

    code added
    - Header added
    - initialized public component name

--- src/project/Schedule.java
+++ src/project/Schedule.java
@@ -1,5 +1,13 @@
 package project;
 
+/* Name: Tiara, William, Tarik
+ * Date: 2026.01.12
+ * Description: creating a the save and load method.
+ */
+
+
 public class Schedule {
+	
+	public String name;
 
 }
