21f900db5bcf7a6209ede6aeac9b96b288445394
William <williamdu488@gmail.com>
2026-01-13 15:38:10 -0500

    a lot more getters/setters/attributes

--- src/project/Assignment.java
+++ src/project/Assignment.java
@@ -1,33 +1,79 @@
 package project;
 
+import java.util.ArrayList;
+
 /*
  * Name(s): Tiara, Tarik, William
  * Date: 2026.01.13
- * Description: creating the assignment class
+ * Description: Assignment links together a course with its teacher, room, time, and students
  */
 
 public class Assignment {
-
+	
 	// Attribute
-
-	public String id;
-
-	// Constructor
-
-	public Assignment(String id) {
-		this.id = id;
-	}
-
-	// Getter method
-
-	public String getId() {
-		return id;
-	}
-
-	// Setter method
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
+    
+    private String id;
+    private Course course;
+    private Teacher teacher;
+    private Room room;
+    private TimeBlock timeBlock;
+    private ArrayList<Student> students;
+    
+    // Constructor
+    
+    public Assignment(String id, Course course, Teacher teacher, Room room, TimeBlock timeBlock) {
+        this.id = id;
+        this.course = course;
+        this.teacher = teacher;
+        this.room = room;
+        this.timeBlock = timeBlock;
+        this.students = new ArrayList<Student>();
+    }
+    
+    // Getters
+    
+    public String getId() {
+        return id;
+    }
+    
+    public Course getCourse() {
+        return course;
+    }
+    
+    public Teacher getTeacher() {
+        return teacher;
+    }
+    
+    public Room getRoom() {
+        return room;
+    }
+    
+    public TimeBlock getTimeBlock() {
+        return timeBlock;
+    }
+    
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+    
+    // Setters
+    
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+    
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+    
+    public void setTimeBlock(TimeBlock timeBlock) {
+        this.timeBlock = timeBlock;
+    }
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+    
+    public void removeStudent(Student student) {
+        students.remove(student);
+    }
 }
