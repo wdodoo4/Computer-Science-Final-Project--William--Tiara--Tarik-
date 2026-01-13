afa5ce9ab12301b6328836ced405dfccf3e68d08
Tiara <350704334@gapps.yrdsb.ca>
2026-01-13 09:22:02 -0500

    Schedule class coded
    Attribute, constructor, and Getter/Setter methods coded in schedule class.

--- src/project/Schedule.java
+++ src/project/Schedule.java
@@ -7,7 +7,26 @@ package project;
 
 public class Schedule {
 
-	// public String name;
+	// Attributes
 
+	public String name;
 
+	// constructor
+
+	public Schedule(String name) {
+		this.name = name;
 	}
+
+	// Getter method
+
+	public String getName() {
+		return name;
+	}
+
+	// Setter method
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
