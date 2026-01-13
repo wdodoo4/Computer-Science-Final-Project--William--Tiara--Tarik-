9d0ce8c676e07d219149bd48189f19dae507ff2b
William <williamdu488@gmail.com>
2026-01-13 15:43:34 -0500

    added id attribute for timeblock

--- src/project/TimeBlock.java
+++ src/project/TimeBlock.java
@@ -1,39 +1,44 @@
 package project;
 
 /*
- * Name: Tiara,Tarik,William
+ * Name: Tiara, Tarik, William
  * Date: 2026.01.13
- * Description: Coding TimeBlcok
+ * Description: TimeBlock is a specific time slot like "Monday 9:00-10:00"
  */
 
 public class TimeBlock {
-
-	// Attributes
-
-	public String day;
-	public String startTime;
-	public String endTime;
-
-	// Constructor
-
-	public TimeBlock(String day, String startTime, String endTime) {
-		this.day = day;
-		this.startTime = startTime;
-		this.endTime = endTime;
-	}
-
-	// Getter method
-
-	public String getDay() {
-		return day;
-	}
-
-	public String getStartDay() {
-		return startTime;
-	}
-
-	public String getEndTime() {
-		return endTime;
-	}
-
+    
+    // Attributes
+	
+    private String id;
+    private String day;
+    private String startTime;
+    private String endTime;
+    
+    // Constructor
+    
+    public TimeBlock(String id, String day, String startTime, String endTime) {
+        this.id = id;
+        this.day = day;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+    
+    // Getters
+    
+    public String getId() {
+        return id;
+    }
+    
+    public String getDay() {
+        return day;
+    }
+    
+    public String getStartTime() {
+        return startTime;
+    }
+    
+    public String getEndTime() {
+        return endTime;
+    }
 }
