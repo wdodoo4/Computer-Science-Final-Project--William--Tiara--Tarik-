711c9822b68964412037330210b42172623d75ec
William <williamdu488@gmail.com>
2026-01-13 15:44:31 -0500

    changed student id and name to private variables

--- src/project/Student.java
+++ src/project/Student.java
@@ -3,31 +3,26 @@ package project;
 /*
  * Name: Tiara, Tarik, William
  * Date: 2026.01.13
- * Description: Coding Student class
+ * Description: Student class
  */
 
 public class Student {
-
-	// Attributes
-
-	public String studentId;
-	public String fullName;
-
-	// constructor
-
-	public Student(String studentId, String fullName) {
-		this.studentId = studentId;
-		this.fullName = fullName;
-	}
-
-	// Getter method
-
-	public String getStudentId() {
-		return studentId;
-	}
-
-	public String getFullName() {
-		return fullName;
-	}
-
+    
+    // changed from public to private
+	
+    private String studentId;
+    private String fullName;
+    
+    public Student(String studentId, String fullName) {
+        this.studentId = studentId;
+        this.fullName = fullName;
+    }
+    
+    public String getStudentId() {
+        return studentId;
+    }
+    
+    public String getFullName() {
+        return fullName;
+    }
 }
